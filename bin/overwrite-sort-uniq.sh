18912acde39e4a68e97130edd3e4fc452f4fe15d
Mako <mnfeconicu41@gmail.com>
2016-05-09 22:34:12 +0900

    sort by line length

--- bin/overwrite-sort-uniq.sh
+++ bin/overwrite-sort-uniq.sh
@@ -6,7 +6,7 @@ function sort_uniq {
   local file=$1
   local tmpfile="/tmp/$(basename $file)"
   trap "rm \"$tmpfile\"; exit 1" 2
-  sort "$file" | uniq > "$tmpfile"
+  sort "$file" | uniq | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- > "$tmpfile"
   mv "$tmpfile" "$file"
 }
 
