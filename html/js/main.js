a3d782cee4b79983cfd6924d05234ac8b1a0ebb5
Mako <mnfeconicu41@gmail.com>
2016-05-12 21:10:41 +0900

    make it readable

--- html/js/main.js
+++ html/js/main.js
@@ -196,18 +196,11 @@ for(var i = 0; pair[i]; i++) {
 // load database
 var lang = arg.lang || "js";
 var comments = [];
-var getDatabase;
-try {
-  console.time("load file");
-  getDatabase = jQuery.get("database/" + lang);
-} catch (e) {
-  $('#react').text('error: ' + e.statusText);
-  console.log(e);
-}
-getDatabase
+console.time("load file");
+var getDatabase = jQuery.get("database/" + lang)
   .done(function (data) {
     console.timeEnd("load file");
-    Array.prototype.push.apply(comments, data.split("\n"));
+    comments = data.split("\n");
   })
   .fail(function (e) {
     $('#react').text('error: ' + e.statusText);
